b9a0c551f52b585d50dc051caa38f03c2da4b37b
E A <elena7263827+py@gmail.com>
2019-03-06 18:32:38 +0300

    Names

---
+++ findperson.py
@@ -0,0 +1,12 @@
+name_list = ["вася", "маша", "петя", "валера", "саша", "даша"]
+
+
+def find_person():
+    name = input("Введите имя: ").lower()
+    if name in name_list:
+        print(f"Имя {name} нашлось!")        
+    else:
+        print("Нет такого имени")
+
+while True:
+    find_person()
